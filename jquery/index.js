cf78677c0ad37b24dc7ba5df27d160e8f9768637
amanbhala <amanbhala813@gmail.com>
2022-06-07 01:18:18 +0530

    Add files via upload

---
+++ jquery/index.js
@@ -0,0 +1,5 @@
+// alert("It's working");
+// Here we wait till the jquery library has finished loading and then we are using it. 
+$(document).ready(function(){
+    $("h1").css("color","red");
+});
